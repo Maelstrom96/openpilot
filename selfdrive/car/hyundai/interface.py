9d9845b01c5375aa4920c5af5b62adb8e85ffe61
SiGmA_X666 <sigma666@gmail.com>
2019-11-28 13:47:24 -0800

    Interface update

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -314,7 +314,7 @@ class CarInterface(CarInterfaceBase):
     
     # Fix for Genesis hard fault when steer request sent while the speed is low 
     #enable = 0 if self.CS.v_ego < self.CP.minSteerSpeed and self.CP.carFingerprint == CAR.GENESIS else c.enabled
-    enable = 0 if self.CS.v_ego < self.CP.minSteerSpeed else c.enabled
+    enable = 0 if self.CS.v_ego < self.CP.minSteerSpeed and (self.CP.carFingerprint == CAR.GENESIS or self.CP.minSteerSpeed > 0) else c.enabled
     
     can_sends = self.CC.update(enable, self.CS, self.frame, c.actuators,
                                c.cruiseControl.cancel, c.hudControl.visualAlert, c.hudControl.leftLaneVisible,

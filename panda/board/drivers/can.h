716180d432f80f07617a448f65a4236cbfef810e
Alexandre-Jacques St-Jacques <alexandrovich8@live.ca>
2019-12-04 19:30:01 -0500

    CLU11 mdps spoof

--- panda/board/drivers/can.h
+++ panda/board/drivers/can.h
@@ -399,6 +399,9 @@ void CAN3_RX0_IRQHandler(void) { can_rx(2); }
 void CAN3_SCE_IRQHandler(void) { can_sce(CAN3); }
 
 void can_send(CAN_FIFOMailBox_TypeDef *to_push, uint8_t bus_number) {
+  // insert the proper bus value
+  to_push->RDTR = (to_push->RDTR & 0xFFFFF00F) | bus_number << 4;
+  
   if (safety_tx_hook(to_push) != 0) {
     if (bus_number < BUS_MAX) {
       // add CAN packet to send queue

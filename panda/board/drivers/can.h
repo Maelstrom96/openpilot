34821907c3e048b9afc62ab8439005c7cac06436
Alexandre-Jacques St-Jacques <alexandrovich8@live.ca>
2019-12-13 00:17:01 -0500

    Hacks I didn't want to write

--- panda/board/drivers/can.h
+++ panda/board/drivers/can.h
@@ -221,6 +221,11 @@ void can_set_gmlan(uint8_t bus) {
 
 // TODO: remove
 void can_set_obd(uint8_t harness_orientation, bool obd){
+  // Always have the Panda in OBD mode
+  if (default_safety_mode == SAFETY_HYUNDAI_PUF) {
+    obd = true;
+  }
+  
   if(obd){
     puts("setting CAN2 to be OBD\n");
   } else {

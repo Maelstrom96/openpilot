d80e4022978b5349b98c7c6cc7b035eb95fa63a7
Alexandre-Jacques St-Jacques <alexandrovich8@live.ca>
2019-12-15 20:20:53 -0500

    Added Anti-Nag with MDPS harness

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -60,73 +60,74 @@ static int nooutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
   int bus_fwd = -1;
 
   if (HKG_forwarding_enabled) {
-    if (bus_num == 0) {
-      // MDPS12
-      // Code for LKA/LFA/HDA anti-nagging.
-      if (addr == 593) {
-        uint8_t dat[8];
-        int New_Chksum2 = 0;
-        for (int i=0; i<8; i++) {
-          dat[i] = GET_BYTE(to_fwd, i);
+    // MDPS12
+    // Code for LKA/LFA/HDA anti-nagging.
+    if ((bus_num == 0 || bus_num == HKG_MDPS_CAN) && addr == 593) {
+      uint8_t dat[8];
+      int New_Chksum2 = 0;
+      for (int i=0; i<8; i++) {
+        dat[i] = GET_BYTE(to_fwd, i);
+      }
+      if (HKG_MDPS12_cnt > 330) {
+        int StrColTq = dat[0] | (dat[1] & 0x7) << 8;
+        int OutTq = dat[6] >> 4 | dat[7] << 4;
+        if (HKG_MDPS12_cnt == 331) {
+          StrColTq -= 164;
         }
-        if (HKG_MDPS12_cnt > 330) {
-          int StrColTq = dat[0] | (dat[1] & 0x7) << 8;
-          int OutTq = dat[6] >> 4 | dat[7] << 4;
-          if (HKG_MDPS12_cnt == 331) {
-            StrColTq -= 164;
-          }
-          else {
-            StrColTq = HKG_last_StrColT + 34;
-          }
-          OutTq = 2058;
+        else {
+          StrColTq = HKG_last_StrColT + 34;
+        }
+        OutTq = 2058;
 
-          dat[0] = StrColTq & 0xFF;
-          dat[1] &= 0xF8;
-          dat[1] |= StrColTq >> 8;
-          dat[6] &= 0xF;
-          dat[6] |= (OutTq & 0xF) << 4;
-          dat[7] = OutTq >> 4;
-            
+        dat[0] = StrColTq & 0xFF;
+        dat[1] &= 0xF8;
+        dat[1] |= StrColTq >> 8;
+        dat[6] &= 0xF;
+        dat[6] |= (OutTq & 0xF) << 4;
+        dat[7] = OutTq >> 4;
+          
 
-          to_fwd->RDLR &= 0xFFF800;
-          to_fwd->RDLR |= StrColTq;
-          to_fwd->RDHR &= 0xFFFFF;
-          to_fwd->RDHR |= OutTq << 20;
-          HKG_last_StrColT = StrColTq;
+        to_fwd->RDLR &= 0xFFF800;
+        to_fwd->RDLR |= StrColTq;
+        to_fwd->RDHR &= 0xFFFFF;
+        to_fwd->RDHR |= OutTq << 20;
+        HKG_last_StrColT = StrColTq;
 
-          dat[3] = 0;
-          if (HKG_MDPS12_checksum == 0) { 
-            for (int i=0; i<8; i++) {
-              New_Chksum2 += dat[i];
-            }
-            New_Chksum2 %= 256;
+        dat[3] = 0;
+        if (HKG_MDPS12_checksum == 0) { 
+          for (int i=0; i<8; i++) {
+            New_Chksum2 += dat[i];
           }
-          else if (HKG_MDPS12_checksum == 1) { //we need CRC8 checksum
-            uint8_t crc = 0xFF;
-            uint8_t poly = 0x1D;
-            int i, j;
-            for (i=0; i<8; i++){
-              if (i!=3){ //don't include CRC byte
-                crc ^= dat[i];
-                for (j=0; j<8; j++) {
-                  if ((crc & 0x80) != 0U) {
-                    crc = (crc << 1) ^ poly;
-                  } else
-                  {
-                  crc <<= 1;
-                  }
+          New_Chksum2 %= 256;
+        }
+        else if (HKG_MDPS12_checksum == 1) { //we need CRC8 checksum
+          uint8_t crc = 0xFF;
+          uint8_t poly = 0x1D;
+          int i, j;
+          for (i=0; i<8; i++){
+            if (i!=3){ //don't include CRC byte
+              crc ^= dat[i];
+              for (j=0; j<8; j++) {
+                if ((crc & 0x80) != 0U) {
+                  crc = (crc << 1) ^ poly;
+                } else
+                {
+                crc <<= 1;
                 }
               }
             }
-            crc ^= 0xFF;
-            crc %= 256;
-            New_Chksum2 = crc;
           }
-          to_fwd->RDLR |= New_Chksum2 << 24;
+          crc ^= 0xFF;
+          crc %= 256;
+          New_Chksum2 = crc;
         }
-        HKG_MDPS12_cnt += 1;
-        HKG_MDPS12_cnt %= 345;
+        to_fwd->RDLR |= New_Chksum2 << 24;
       }
+      HKG_MDPS12_cnt += 1;
+      HKG_MDPS12_cnt %= 345;
+    }
+    
+    if (bus_num == 0) {
       bus_fwd = 2;
       // Forward to MDPS
       (*fwd_bus)[0] = HKG_MDPS_CAN;
